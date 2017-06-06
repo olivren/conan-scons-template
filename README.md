13a8c67eb875eabb2e98f369a730080451df323c
orenaud <orenaud@coventor.com>
2017-06-06 15:26:32 +0200

    Add comments, and clarify some aspects of the code

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # Conan SCons template
 
 A template for a [Conan package manager][1] recipe, that uses [SCons][2] as its build tool instead of CMake.
-It supports MSVC 2017 Pro and GCC, with a release and a debug build type.
+
+This template demonstrates two different uses of SCons when writing a Conan recipe:
+- Use SCons in the `test_package` part (the part that compiles a sample project to check that
+  the library is correctly packaged and can be linked against)
+- Use SCons to compile the library being packaged.
+
+These two parts are independant one another. For example, it is perfectly fine to package a library
+built with a Makefile, and use SCons for the `test_package` part.
+
+This template supports Windows (MSVC) and Linux (GCC), and it supports a release and a debug build type.
+The version of MSVC is hardcoded, and the coherence between the compiler setting of Conan (compiler, version
+and arch), and the compiler detected by SCons is not checked.
 
 Inside the root directory, just run `conan test_package` or `conan test_package -s build_type=Debug`.
 
--- conanfile.py
+++ conanfile.py
@@ -2,26 +2,39 @@ from conans import ConanFile, tools
 import os
 
 class HelloConan(ConanFile):
+    
+    # Attributes of the Hello library, that you should specify in any case
     name = 'Hello'
     version = '1.0'
     license = 'MIT'
     url = ''
     settings = 'os', 'compiler', 'build_type', 'arch'
-    description = 'myproj description'
-    exports_sources = "src/*"
-    generators = "scons"
+    description = 'Hello lib description'
+    
+    # All the code below this comment is an example of how you can compile the
+    # Hello library itself using the SCons build tool. If the library you want to
+    # package is not based on SCons, just delete everything under this comment
+    # and also the content of the directory `src`.
+    
+    # Use this attribute only if the source code of the Hello library is not
+    # external, and lives next to the recipe.
+    exports_sources = 'src/*'
+    
+    # This is useful only if the Hello library has dependencies on other Conan
+    # packages.
+    generators = 'scons'
 
     def build(self):
         debug_opt = '--debug-build' if self.settings.build_type == 'Debug' else ''
-        os.makedirs("build")
-        # FIXME: Compiler, version, arch are hardcoded, not parametrized
-        with tools.chdir("build"):
+        os.makedirs('build')
+        # FIXME: Compiler, version, arch are hardcoded, not parameterized
+        with tools.chdir('build'):
             self.run('scons -C {}/src {}'.format(self.source_folder, debug_opt))
         
     def package(self):
-        self.copy("*.h", "include", src="src")
-        self.copy("*.lib", "lib", keep_path=False)
-        self.copy("*.a", "lib", keep_path=False)
+        self.copy('*.h', 'include', src='src')
+        self.copy('*.lib', 'lib', keep_path=False)
+        self.copy('*.a', 'lib', keep_path=False)
         
     def package_info(self):
-        self.cpp_info.libs = ["hello"]
+        self.cpp_info.libs = ['hello']
